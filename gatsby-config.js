219a7c343723a013aace0587db59f13cf456a2df
Sara Vieira <hey@iamsaravieira.com>
2018-12-04 17:32:30 +0100

    add a11y plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,17 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-accessibilityjs`,
+      options: {
+        injectStyles: `
+        .accessibility-error {
+          border: 3px solid #f00;
+        }
+      `,
+        errorClassName: `accessibility-error`
+      }
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`
